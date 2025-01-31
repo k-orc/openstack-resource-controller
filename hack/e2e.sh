9c7ddab408bc9a567ce638917126d7fdb1b39819
Martin André <m.andre@redhat.com>
2025-01-31 11:31:57 +0100

    CI: Allows to override cloud names in tests
    We can now export the `E2E_OPENSTACK_CLOUD_NAME` and
    `E2E_OPENSTACK_ADMIN_CLOUD_NAME` variables to override the default
    credentials to use for tenant and admin operations during tests.
    
    These default to `devstack` and `devstack-admin-demo` respectively.
    
    Fixes #207

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -12,10 +12,6 @@ if [ -n "$E2E_CACERT" ]; then
     E2E_KUTTL_CACERT_OPT="--from-file=cacert=${E2E_CACERT}"
 fi
 
-# Export variables referenced in kuttl tests.
-export E2E_KUTTL_OSCLOUDS
-export E2E_KUTTL_CACERT_OPT
-
 # Run kuttl tests from a specific directory.
 # Defaults to empty string (all discovered kuttl directories)
 E2E_KUTTL_DIR=${E2E_KUTTL_DIR:-}
@@ -26,6 +22,36 @@ E2E_KUTTL_TEST=${E2E_KUTTL_TEST:-}
 
 # Define a custom external network
 export E2E_EXTERNAL_NETWORK_NAME=${E2E_EXTERNAL_NETWORK_NAME:-private}
+
+creds_dir=$(mktemp -d)
+
+function logresources() {
+    # Log all resources if exiting with an error
+    if [ $? != 0 ]; then
+        kubectl get openstack -o yaml -A
+    fi
+}
+
+function cleanup() {
+    # logresources must be called first as it checks the exit code
+    logresources
+    rm -rf -- "$creds_dir"
+}
+
+trap cleanup EXIT
+
+# Export variables referenced in kuttl tests.
+export E2E_KUTTL_OSCLOUDS="${creds_dir}/clouds.yaml"
+export E2E_KUTTL_CACERT_OPT
+
+# Name of the openstack credentials to use from the E2E_OSCLOUDS file
+E2E_OPENSTACK_CLOUD_NAME=${E2E_OPENSTACK_CLOUD_NAME:-devstack}
+# Name of the openstack admin credentials to use from the E2E_OSCLOUDS file
+E2E_OPENSTACK_ADMIN_CLOUD_NAME=${E2E_OPENSTACK_ADMIN_CLOUD_NAME:-"devstack-admin-demo"}
+
+cp "${E2E_OSCLOUDS}" "${E2E_KUTTL_OSCLOUDS}"
+sed -r -i "s/^(\s+)${E2E_OPENSTACK_CLOUD_NAME}:/\1openstack:/g" "${E2E_KUTTL_OSCLOUDS}"
+sed -r -i "s/^(\s+)${E2E_OPENSTACK_ADMIN_CLOUD_NAME}:/\1openstack-admin:/g" "${E2E_KUTTL_OSCLOUDS}"
 
 kubectl kuttl test $E2E_KUTTL_DIR --test "$E2E_KUTTL_TEST"
 
@@ -38,14 +64,6 @@ cd examples
 sed "s/  devstack:/  openstack:/g" /etc/openstack/clouds.yaml > local-config/clouds.yaml
 envsubst < local-config/external-network-filter.yaml.example > local-config/external-network-filter.yaml
 make local-config
-
-function logresources() {
-    # Log all resources if exiting with an error
-    if [ $? != 0 ]; then
-        kubectl get openstack -o yaml -A
-    fi
-}
-trap logresources EXIT
 
 # Apply the cirros server example and wait for the server to be available
 kubectl apply -k apply/cirros --server-side
