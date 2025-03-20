1fddbc0681d6c7817488e068588273740cf33614
Martin André <m.andre@redhat.com>
2025-03-20 16:47:56 +0100

    server: Cleanup flavor dependency progress status
    The status should report that it is waiting for the resource to be
    created or to be ready, but not both at the same time.

--- internal/controllers/server/actuator.go
+++ internal/controllers/server/actuator.go
@@ -132,8 +132,7 @@ func (actuator serverActuator) CreateResource(ctx context.Context, obj *orcv1alp
 			} else {
 				return nil, nil, fmt.Errorf("fetching flavor %s: %w", flavorKey.Name, err)
 			}
-		}
-		if !orcv1alpha1.IsAvailable(flavor) || flavor.Status.ID == nil {
+		} else if !orcv1alpha1.IsAvailable(flavor) || flavor.Status.ID == nil {
 			progressStatus = append(progressStatus, progress.WaitingOnORCReady("Flavor", flavorKey.Name))
 		}
 	}
