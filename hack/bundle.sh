5f6212c46f3266b9dd6ddec6bb389fa467b14ea0
Martin André <m.andre@redhat.com>
2025-08-13 12:04:57 +0200

    Bundle: use the correct go.kubebuilder.io/v4 plugin
    The latest operator-sdk ships with a `go.kubebuilder.io/v4` plugin, and
    not a `go.kubebuilder.io/v4-alpha` one.

--- hack/bundle.sh
+++ hack/bundle.sh
@@ -6,6 +6,6 @@ IMAGE=${BASE_IMAGE:-openstack-resource-controller}
 TAG=${BASE_IMAGE:-v2.2.0}
 IMG=${REGISTRY}/${IMAGE}:${TAG}
 
-operator-sdk generate kustomize manifests -q --plugins=go.kubebuilder.io/v4-alpha 
+operator-sdk generate kustomize manifests -q --plugins=go.kubebuilder.io/v4
 cd config/manager && kustomize edit set image controller=${IMG} && cd ../..
-kustomize build config/manifests | operator-sdk generate bundle --plugins=go.kubebuilder.io/v4-alpha --use-image-digests
+kustomize build config/manifests | operator-sdk generate bundle --plugins=go.kubebuilder.io/v4 --use-image-digests
