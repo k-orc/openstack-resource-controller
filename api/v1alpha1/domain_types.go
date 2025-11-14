67644e7cd64175ebcdcf658ca82169cac90b3a4d
Daniel Lawton <dlawton@redhat.com>
2025-11-14 14:51:04 +0000

    Created the Skeleton of the Domain Controller using the controller generator:
    	> go run ./cmd/scaffold-controller \
               -interactive=false \
    	   -kind Domain \
               -gophercloud-client NewIdentityV3 \
    	   -gophercloud-module github.com/gophercloud/gophercloud/v2/openstack/identity/v3/domains
    
    Signed-off-by: Daniel Lawton <dlawton@redhat.com>

---
+++ api/v1alpha1/domain_types.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2025 The ORC Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+// DomainResourceSpec contains the desired state of the resource.
+type DomainResourceSpec struct {
+	// name will be the name of the created resource. If not specified, the
+	// name of the ORC object will be used.
+	// +optional
+	Name *OpenStackName `json:"name,omitempty"`
+
+	// description is a human-readable description for the resource.
+	// +kubebuilder:validation:MinLength:=1
+	// +kubebuilder:validation:MaxLength:=255
+	// +optional
+	Description *string `json:"description,omitempty"`
+
+	// TODO(scaffolding): Add more types.
+	// To see what is supported, you can take inspiration from the CreateOpts stucture from
+	// github.com/gophercloud/gophercloud/v2/openstack/identity/v3/domains
+	//
+	// Until you have implemented mutability for the field, you must add a CEL validation
+	// preventing the field being modified:
+	// `// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="<fieldname> is immutable"`
+}
+
+// DomainFilter defines an existing resource by its properties
+// +kubebuilder:validation:MinProperties:=1
+type DomainFilter struct {
+	// name of the existing resource
+	// +optional
+	Name *OpenStackName `json:"name,omitempty"`
+
+	// description of the existing resource
+	// +kubebuilder:validation:MinLength:=1
+	// +kubebuilder:validation:MaxLength:=255
+	// +optional
+	Description *string `json:"description,omitempty"`
+
+	// TODO(scaffolding): Add more types.
+	// To see what is supported, you can take inspiration from the ListOpts stucture from
+	// github.com/gophercloud/gophercloud/v2/openstack/identity/v3/domains
+}
+
+// DomainResourceStatus represents the observed state of the resource.
+type DomainResourceStatus struct {
+	// name is a Human-readable name for the resource. Might not be unique.
+	// +kubebuilder:validation:MaxLength=1024
+	// +optional
+	Name string `json:"name,omitempty"`
+
+	// description is a human-readable description for the resource.
+	// +kubebuilder:validation:MaxLength=1024
+	// +optional
+	Description string `json:"description,omitempty"`
+
+	// TODO(scaffolding): Add more types.
+	// To see what is supported, you can take inspiration from the Domain stucture from
+	// github.com/gophercloud/gophercloud/v2/openstack/identity/v3/domains
+}
