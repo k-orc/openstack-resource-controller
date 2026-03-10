392b9054788280ac10b8b378c3c5dc030bab729c
Gondermann <gondermann@b1-systems.de>
2026-03-10 15:59:57 +0100

    Scaffolding for the ApplicationCredential controller
    $ go run ./cmd/scaffold-controller -interactive=false \
        -kind=ApplicationCredential \
        -gophercloud-client=NewIdentityV3 \
        -gophercloud-module=github.com/gophercloud/gophercloud/v2/openstack/identity/v3/applicationcredentials \
        -gophercloud-type=ApplicationCredential \
        -openstack-json-object=application_credentials \
        -required-create-dependency=User \
        -import-dependency=User
    
    On-behalf-of: SAP nils.gondermann@sap.com

---
+++ internal/controllers/applicationcredential/actuator_test.go
@@ -0,0 +1,119 @@
+/*
+Copyright The ORC Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package applicationcredential
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/applicationcredentials"
+	orcv1alpha1 "github.com/k-orc/openstack-resource-controller/v2/api/v1alpha1"
+	"k8s.io/utils/ptr"
+)
+
+func TestNeedsUpdate(t *testing.T) {
+	testCases := []struct {
+		name         string
+		updateOpts   applicationcredentials.UpdateOpts
+		expectChange bool
+	}{
+		{
+			name:         "Empty base opts",
+			updateOpts:   applicationcredentials.UpdateOpts{},
+			expectChange: false,
+		},
+		{
+			name:         "Updated opts",
+			updateOpts:   applicationcredentials.UpdateOpts{Name: ptr.To("updated")},
+			expectChange: true,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := needsUpdate(tt.updateOpts)
+			if got != tt.expectChange {
+				t.Errorf("Expected change: %v, got: %v", tt.expectChange, got)
+			}
+		})
+	}
+}
+
+func TestHandleNameUpdate(t *testing.T) {
+	ptrToName := ptr.To[orcv1alpha1.OpenStackName]
+	testCases := []struct {
+		name          string
+		newValue      *orcv1alpha1.OpenStackName
+		existingValue string
+		expectChange  bool
+	}{
+		{name: "Identical", newValue: ptrToName("name"), existingValue: "name", expectChange: false},
+		{name: "Different", newValue: ptrToName("new-name"), existingValue: "name", expectChange: true},
+		{name: "No value provided, existing is identical to object name", newValue: nil, existingValue: "object-name", expectChange: false},
+		{name: "No value provided, existing is different from object name", newValue: nil, existingValue: "different-from-object-name", expectChange: true},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			resource := &orcv1alpha1.ApplicationCredential{}
+			resource.Name = "object-name"
+			resource.Spec = orcv1alpha1.ApplicationCredentialSpec{
+				Resource: &orcv1alpha1.ApplicationCredentialResourceSpec{Name: tt.newValue},
+			}
+			osResource := &osResourceT{Name: tt.existingValue}
+
+			updateOpts := applicationcredentials.UpdateOpts{}
+			handleNameUpdate(&updateOpts, resource, osResource)
+
+			got, _ := needsUpdate(updateOpts)
+			if got != tt.expectChange {
+				t.Errorf("Expected change: %v, got: %v", tt.expectChange, got)
+			}
+		})
+
+	}
+}
+
+func TestHandleDescriptionUpdate(t *testing.T) {
+	ptrToDescription := ptr.To[string]
+	testCases := []struct {
+		name          string
+		newValue      *string
+		existingValue string
+		expectChange  bool
+	}{
+		{name: "Identical", newValue: ptrToDescription("desc"), existingValue: "desc", expectChange: false},
+		{name: "Different", newValue: ptrToDescription("new-desc"), existingValue: "desc", expectChange: true},
+		{name: "No value provided, existing is set", newValue: nil, existingValue: "desc", expectChange: true},
+		{name: "No value provided, existing is empty", newValue: nil, existingValue: "", expectChange: false},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			resource := &orcv1alpha1.ApplicationCredentialResourceSpec{Description: tt.newValue}
+			osResource := &osResourceT{Description: tt.existingValue}
+
+			updateOpts := applicationcredentials.UpdateOpts{}
+			handleDescriptionUpdate(&updateOpts, resource, osResource)
+
+			got, _ := needsUpdate(updateOpts)
+			if got != tt.expectChange {
+				t.Errorf("Expected change: %v, got: %v", tt.expectChange, got)
+			}
+		})
+
+	}
+}
---
+++ test/apivalidations/applicationcredential_test.go
@@ -0,0 +1,128 @@
+/*
+Copyright The ORC Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apivalidations
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	orcv1alpha1 "github.com/k-orc/openstack-resource-controller/v2/api/v1alpha1"
+	applyconfigv1alpha1 "github.com/k-orc/openstack-resource-controller/v2/pkg/clients/applyconfiguration/api/v1alpha1"
+)
+
+const (
+	applicationcredentialName = "applicationcredential"
+	applicationcredentialID   = "265c9e4f-0f5a-46e4-9f3f-fb8de25ae120"
+)
+
+func applicationcredentialStub(namespace *corev1.Namespace) *orcv1alpha1.ApplicationCredential {
+	obj := &orcv1alpha1.ApplicationCredential{}
+	obj.Name = applicationcredentialName
+	obj.Namespace = namespace.Name
+	return obj
+}
+
+func testApplicationCredentialResource() *applyconfigv1alpha1.ApplicationCredentialResourceSpecApplyConfiguration {
+	return applyconfigv1alpha1.ApplicationCredentialResourceSpec().
+		WithUserRef("user")
+}
+
+func baseApplicationCredentialPatch(obj client.Object) *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration {
+	return applyconfigv1alpha1.ApplicationCredential(obj.GetName(), obj.GetNamespace()).
+		WithSpec(applyconfigv1alpha1.ApplicationCredentialSpec().
+			WithCloudCredentialsRef(testCredentials()))
+}
+
+func testApplicationCredentialImport() *applyconfigv1alpha1.ApplicationCredentialImportApplyConfiguration {
+	return applyconfigv1alpha1.ApplicationCredentialImport().WithID(applicationcredentialID)
+}
+
+var _ = Describe("ORC ApplicationCredential API validations", func() {
+	var namespace *corev1.Namespace
+	BeforeEach(func() {
+		namespace = createNamespace()
+	})
+
+	runManagementPolicyTests(func() *corev1.Namespace { return namespace }, managementPolicyTestArgs[*applyconfigv1alpha1.ApplicationCredentialApplyConfiguration]{
+		createObject: func(ns *corev1.Namespace) client.Object { return applicationcredentialStub(ns) },
+		basePatch: func(obj client.Object) *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration {
+			return baseApplicationCredentialPatch(obj)
+		},
+		applyResource: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithResource(testApplicationCredentialResource())
+		},
+		applyImport: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithImport(testApplicationCredentialImport())
+		},
+		applyEmptyImport: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithImport(applyconfigv1alpha1.ApplicationCredentialImport())
+		},
+		applyEmptyFilter: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithImport(applyconfigv1alpha1.ApplicationCredentialImport().WithFilter(applyconfigv1alpha1.ApplicationCredentialFilter()))
+		},
+		applyValidFilter: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithImport(applyconfigv1alpha1.ApplicationCredentialImport().WithFilter(applyconfigv1alpha1.ApplicationCredentialFilter().WithName("foo")))
+		},
+		applyManaged: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithManagementPolicy(orcv1alpha1.ManagementPolicyManaged)
+		},
+		applyUnmanaged: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithManagementPolicy(orcv1alpha1.ManagementPolicyUnmanaged)
+		},
+		applyManagedOptions: func(p *applyconfigv1alpha1.ApplicationCredentialApplyConfiguration) {
+			p.Spec.WithManagedOptions(applyconfigv1alpha1.ManagedOptions().WithOnDelete(orcv1alpha1.OnDeleteDetach))
+		},
+		getManagementPolicy: func(obj client.Object) orcv1alpha1.ManagementPolicy {
+			return obj.(*orcv1alpha1.ApplicationCredential).Spec.ManagementPolicy
+		},
+		getOnDelete: func(obj client.Object) orcv1alpha1.OnDelete {
+			return obj.(*orcv1alpha1.ApplicationCredential).Spec.ManagedOptions.OnDelete
+		},
+	})
+
+	It("should reject a applicationcredential without required fields", func(ctx context.Context) {
+		obj := applicationcredentialStub(namespace)
+		patch := baseApplicationCredentialPatch(obj)
+		patch.Spec.WithResource(applyconfigv1alpha1.ApplicationCredentialResourceSpec())
+		Expect(applyObj(ctx, obj, patch)).NotTo(Succeed())
+	})
+
+	It("should have immutable userRef", func(ctx context.Context) {
+		obj := applicationcredentialStub(namespace)
+		patch := baseApplicationCredentialPatch(obj)
+		patch.Spec.WithResource(testApplicationCredentialResource().
+			WithUserRef("user-a"))
+		Expect(applyObj(ctx, obj, patch)).To(Succeed())
+
+		patch.Spec.WithResource(testApplicationCredentialResource().
+			WithUserRef("user-b"))
+		Expect(applyObj(ctx, obj, patch)).To(MatchError(ContainSubstring("userRef is immutable")))
+	})
+
+	// TODO(scaffolding): Add more resource-specific validation tests.
+	// Some common things to test:
+	// - Immutability of fields with `self == oldSelf` validation
+	// - Enum validation (valid and invalid values)
+	// - Numeric range validation (min/max bounds)
+	// - Tag uniqueness (if the resource has tags with listType=set)
+	// - Format validation (CIDR, UUID, etc.)
+	// - Cross-field validation rules
+})
