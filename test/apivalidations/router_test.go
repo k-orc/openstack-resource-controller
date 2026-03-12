0923cea6c1f2ea376277431c2c803cc006776234
Martin André <m.andre@redhat.com>
2026-03-12 17:34:23 +0100

    test: add API validation tests for Router

---
+++ test/apivalidations/router_test.go
@@ -0,0 +1,204 @@
+/*
+Copyright The ORC Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apivalidations
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	orcv1alpha1 "github.com/k-orc/openstack-resource-controller/v2/api/v1alpha1"
+	applyconfigv1alpha1 "github.com/k-orc/openstack-resource-controller/v2/pkg/clients/applyconfiguration/api/v1alpha1"
+)
+
+const (
+	routerObjName = "router"
+	routerID      = "265c9e4f-0f5a-46e4-9f3f-fb8de25ae132"
+)
+
+func routerStub(namespace *corev1.Namespace) *orcv1alpha1.Router {
+	obj := &orcv1alpha1.Router{}
+	obj.Name = routerObjName
+	obj.Namespace = namespace.Name
+	return obj
+}
+
+func testRouterResource() *applyconfigv1alpha1.RouterResourceSpecApplyConfiguration {
+	return applyconfigv1alpha1.RouterResourceSpec()
+}
+
+func baseRouterPatch(router client.Object) *applyconfigv1alpha1.RouterApplyConfiguration {
+	return applyconfigv1alpha1.Router(router.GetName(), router.GetNamespace()).
+		WithSpec(applyconfigv1alpha1.RouterSpec().
+			WithCloudCredentialsRef(testCredentials()))
+}
+
+func testRouterImport() *applyconfigv1alpha1.RouterImportApplyConfiguration {
+	return applyconfigv1alpha1.RouterImport().WithID(routerID)
+}
+
+var _ = Describe("ORC Router API validations", func() {
+	var namespace *corev1.Namespace
+	BeforeEach(func() {
+		namespace = createNamespace()
+	})
+
+	It("should allow to create a minimal router and managementPolicy should default to managed", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithResource(testRouterResource())
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+		Expect(router.Spec.ManagementPolicy).To(Equal(orcv1alpha1.ManagementPolicyManaged))
+	})
+
+	It("should have immutable externalGateways", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithResource(applyconfigv1alpha1.RouterResourceSpec().
+			WithExternalGateways(applyconfigv1alpha1.ExternalGateway().WithNetworkRef("net-a")))
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+
+		patch.Spec.WithResource(applyconfigv1alpha1.RouterResourceSpec().
+			WithExternalGateways(applyconfigv1alpha1.ExternalGateway().WithNetworkRef("net-b")))
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("externalGateways is immutable")))
+	})
+
+	It("should have immutable distributed", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithResource(applyconfigv1alpha1.RouterResourceSpec().
+			WithDistributed(true))
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+
+		patch.Spec.WithResource(applyconfigv1alpha1.RouterResourceSpec().
+			WithDistributed(false))
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("distributed is immutable")))
+	})
+
+	It("should have immutable projectRef", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithResource(applyconfigv1alpha1.RouterResourceSpec().
+			WithProjectRef("project-a"))
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+
+		patch.Spec.WithResource(applyconfigv1alpha1.RouterResourceSpec().
+			WithProjectRef("project-b"))
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("projectRef is immutable")))
+	})
+
+	It("should reject duplicate tags", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithResource(applyconfigv1alpha1.RouterResourceSpec().
+			WithTags("foo", "bar", "foo"))
+		Expect(applyObj(ctx, router, patch)).NotTo(Succeed())
+	})
+
+	It("should require import for unmanaged", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithManagementPolicy(orcv1alpha1.ManagementPolicyUnmanaged)
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("import must be specified when policy is unmanaged")))
+
+		patch.Spec.WithImport(testRouterImport())
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+	})
+
+	It("should not permit unmanaged with resource", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.
+			WithManagementPolicy(orcv1alpha1.ManagementPolicyUnmanaged).
+			WithImport(testRouterImport()).
+			WithResource(testRouterResource())
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("resource may not be specified when policy is unmanaged")))
+	})
+
+	It("should not permit empty import", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.
+			WithManagementPolicy(orcv1alpha1.ManagementPolicyUnmanaged).
+			WithImport(applyconfigv1alpha1.RouterImport())
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("spec.import in body should have at least 1 properties")))
+	})
+
+	It("should not permit empty import filter", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.
+			WithManagementPolicy(orcv1alpha1.ManagementPolicyUnmanaged).
+			WithImport(applyconfigv1alpha1.RouterImport().
+				WithFilter(applyconfigv1alpha1.RouterFilter()))
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("spec.import.filter in body should have at least 1 properties")))
+	})
+
+	It("should permit import filter with name", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.
+			WithManagementPolicy(orcv1alpha1.ManagementPolicyUnmanaged).
+			WithImport(applyconfigv1alpha1.RouterImport().
+				WithFilter(applyconfigv1alpha1.RouterFilter().WithName("foo")))
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+	})
+
+	It("should require resource for managed", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithManagementPolicy(orcv1alpha1.ManagementPolicyManaged)
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("resource must be specified when policy is managed")))
+
+		patch.Spec.WithResource(testRouterResource())
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+	})
+
+	It("should not permit managed with import", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.
+			WithImport(testRouterImport()).
+			WithManagementPolicy(orcv1alpha1.ManagementPolicyManaged).
+			WithResource(testRouterResource())
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("import may not be specified when policy is managed")))
+	})
+
+	It("should not permit managedOptions for unmanaged", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.
+			WithImport(testRouterImport()).
+			WithManagementPolicy(orcv1alpha1.ManagementPolicyUnmanaged).
+			WithManagedOptions(applyconfigv1alpha1.ManagedOptions().
+				WithOnDelete(orcv1alpha1.OnDeleteDetach))
+		Expect(applyObj(ctx, router, patch)).To(MatchError(ContainSubstring("managedOptions may only be provided when policy is managed")))
+	})
+
+	It("should permit managedOptions for managed", func(ctx context.Context) {
+		router := routerStub(namespace)
+		patch := baseRouterPatch(router)
+		patch.Spec.WithResource(testRouterResource()).
+			WithManagedOptions(applyconfigv1alpha1.ManagedOptions().
+				WithOnDelete(orcv1alpha1.OnDeleteDetach))
+		Expect(applyObj(ctx, router, patch)).To(Succeed())
+		Expect(router.Spec.ManagedOptions.OnDelete).To(Equal(orcv1alpha1.OnDelete("detach")))
+	})
+})
