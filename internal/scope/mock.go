c69e959d777bca71473d87666e1389e75e87d4ce
Gondermann <gondermann@b1-systems.de>
2026-05-06 14:26:45 +0200

    Scaffolding for the RegisteredLimit controller
    Add the OpenStack client to scope
    
    On-behalf-of: SAP nils.gondermann@sap.com

--- internal/scope/mock.go
+++ internal/scope/mock.go
@@ -45,6 +45,7 @@ type MockScopeFactory struct {
 	KeyPairClient               *mock.MockKeyPairClient
 	NetworkClient               *mock.MockNetworkClient
 	RoleClient                  *mock.MockRoleClient
+	RegisteredLimitClient       *mock.MockRegisteredLimitClient
 	ServiceClient               *mock.MockServiceClient
 	UserClient                  *mock.MockUserClient
 	VolumeClient                *mock.MockVolumeClient
@@ -65,6 +66,7 @@ func NewMockScopeFactory(mockCtrl *gomock.Controller) *MockScopeFactory {
 	keypairClient := mock.NewMockKeyPairClient(mockCtrl)
 	networkClient := mock.NewMockNetworkClient(mockCtrl)
 	roleClient := mock.NewMockRoleClient(mockCtrl)
+	registeredLimitClient := mock.NewMockRegisteredLimitClient(mockCtrl)
 	serviceClient := mock.NewMockServiceClient(mockCtrl)
 	userClient := mock.NewMockUserClient(mockCtrl)
 	volumeClient := mock.NewMockVolumeClient(mockCtrl)
@@ -82,6 +84,7 @@ func NewMockScopeFactory(mockCtrl *gomock.Controller) *MockScopeFactory {
 		KeyPairClient:               keypairClient,
 		NetworkClient:               networkClient,
 		RoleClient:                  roleClient,
+		RegisteredLimitClient:       registeredLimitClient,
 		ServiceClient:               serviceClient,
 		UserClient:                  userClient,
 		VolumeClient:                volumeClient,
@@ -150,6 +153,10 @@ func (f *MockScopeFactory) NewGroupClient() (osclients.GroupClient, error) {
 
 func (f *MockScopeFactory) NewRoleClient() (osclients.RoleClient, error) {
 	return f.RoleClient, nil
+}
+
+func (f *MockScopeFactory) NewRegisteredLimitClient() (osclients.RegisteredLimitClient, error) {
+	return f.RegisteredLimitClient, nil
 }
 
 func (f *MockScopeFactory) NewEndpointClient() (osclients.EndpointClient, error) {
